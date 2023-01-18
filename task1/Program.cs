51ee65d2cc928a27a2121203bec3ffe7d2cbfd1d
Ilya <osetia9595@gmail.com>
2023-01-18 21:27:00 +0300

    task2

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("input number");
+int n = int.Parse(Console.ReadLine());
+
+NaturalNumbers(n);
+
+
+void NaturalNumbers (int n)
+{
+    if (n < 0) Console.WriteLine("not a natural number");
+    if (n == 0) return;
+    Console.Write(n + " ");
+    NaturalNumbers (n - 1);
+}
