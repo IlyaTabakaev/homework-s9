6a4f486f8c360d4f17962b652f780220669bd4a6
Ilya <osetia9595@gmail.com>
2023-01-18 21:38:21 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("input M");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("input N");
+int n = int.Parse(Console.ReadLine());
+
+SumNumbers(m, n, 0);
+
+void SumNumbers (int m, int n, int sum)
+{
+    if (m > n)
+    {
+        Console.WriteLine($"sum from m to n is {sum}");
+        return;
+    }
+    sum += m++;
+    SumNumbers(m, n, sum);
+}
