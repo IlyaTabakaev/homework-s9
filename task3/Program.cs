e1261ec4352dd7e4a5884834f0125092f8d8cd7c
Ilya <osetia9595@gmail.com>
2023-01-18 22:11:21 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("input m");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("input n");
+int n = int.Parse(Console.ReadLine());
+
+AkkermanFunc(m, n);
+
+int Func (int m, int n)
+{
+    if (m == 0) 
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+         return Func(m - 1, 1);
+    }
+    else
+    {
+         return Func(m - 1, Func(m, n - 1));
+    }
+}
+
+ void AkkermanFunc(int m, int n)
+ {
+    Console.WriteLine(Func(m, n));
+ }
